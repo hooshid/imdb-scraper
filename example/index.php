986eac21c78780fd4baca5c23607c1bb74dd5bf8
a.kamali <arka1373@gmail.com>
2024-07-13 10:06:01 +0330

    update

--- example/index.php
+++ example/index.php
@@ -17,14 +17,14 @@
         <form action="/example/search.php" method="get">
             <div class="form-group">
                 <label for="search">Search:</label>
-                <input class="form-field" type="text" id="search" name="search" maxlength="50" placeholder="Search or enter title or person id ie:(tt2386490 or nm1706767)">
+                <input class="form-field" type="text" id="search" name="search" maxlength="50" placeholder="Search or enter title or name id ie:(tt2386490 or nm1706767)">
             </div>
 
             <div class="form-group">
                 <label for="type">Type:</label>
                 <select id="type" name="type" class="form-field">
                     <option value="title">Movie</option>
-                    <option value="nm">Person</option>
+                    <option value="name">Name</option>
                     <option value="episode">Episode</option>
                 </select>
             </div>
@@ -39,14 +39,19 @@
         <h2 class="text-center pb-30">Examples</h2>
 
         <div class="menu-links">
-            <b><a href="/example/charts.php">Charts: BoxOffice</a></b>
-            <a href="/example/person.php?id=0000134">Person: Robert De Niro</a>
-            <a href="/example/person.php?id=1297015">Person: Emma Stone</a>
-            <a href="/example/person.php?id=0908094">Person: Paul Walker</a>
-            <a href="/example/title.php?id=0133093">Movie: The Matrix (1999)</a>
-            <a href="/example/title.php?id=3228774">Movie: Cruella (2021)</a>
-            <a href="/example/title.php?id=0944947">TV Series: Game of Thrones (2011-2019)</a>
-            <a href="/example/title.php?id=10048342">TV Mini Series: The Queen's Gambit (2020)</a>
+            <a href="/example/charts.php">Charts: BoxOffice</a>
+
+            <a href="/example/name.php?id=nm0000134">Name: Robert De Niro</a>
+            <a href="/example/name.php?id=nm1297015">Name: Emma Stone</a>
+            <a href="/example/name.php?id=nm0908094">Name: Paul Walker</a>
+
+            <a href="/example/name-search.php?name=Emma">Search name: Emma</a>
+            <a href="/example/name-search.php?birth_monthday=<?php echo date("m-d"); ?>">Search name: Born today</a>
+
+            <a href="/example/title.php?id=tt0133093">Movie: The Matrix (1999)</a>
+            <a href="/example/title.php?id=tt3228774">Movie: Cruella (2021)</a>
+            <a href="/example/title.php?id=tt0944947">TV Series: Game of Thrones (2011-2019)</a>
+            <a href="/example/title.php?id=tt10048342">TV Mini Series: The Queen's Gambit (2020)</a>
         </div>
     </div>
 
--- example/search.php
+++ example/search.php
@@ -4,27 +4,21 @@ require __DIR__ . "/../vendor/autoload.php";
 
 // check search string is imdb id of Title or Person
 $search = trim(strip_tags($_GET["search"]));
-if (!empty($search) && preg_match('/^(tt|nm|)([0-9]+)$/', $search, $matches)) {
-    $type = !empty($matches[1]) ? $matches[1] : $_GET["type"];
-    switch ($type) {
-        case "nm":
-            header("Location: person.php?id=" . $matches[2]);
-            break;
-        default:
-            header("Location: title.php?id=" . $matches[2]);
-            break;
-    }
-    return;
-}
 
-// if we have no search, go back to search page
-if (empty($_GET["search"])) {
+// if search input is empty, go back to example page
+if (empty($search)) {
     header("Location: /example");
     exit;
-}
-
-// search person
-if ($_GET['type'] === 'nm') {
-    header("Location: person-search.php?name=" . $search);
+} // check name id
+elseif (preg_match('/^nm\d+$/', $search)) {
+    header("Location: name.php?id=" . $search);
+    return;
+} // check title id
+elseif (preg_match('/^tt\d+$/', $search)) {
+    header("Location: title.php?id=" . $search);
+    return;
+} // search type is name
+elseif ($_GET['type'] === 'name') {
+    header("Location: name-search.php?name=" . $search);
     exit;
 }
